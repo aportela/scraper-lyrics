0d56214fa0ee15651653588e6eac9f19d632ef83
alex <766f6964+github@gmail.com>
2023-10-02 12:20:41 +0200

    removed duplicated call copied&pasted from bing test

--- src/Test/SourceProviders/LyricsManiaTest.php
+++ src/Test/SourceProviders/LyricsManiaTest.php
@@ -16,13 +16,6 @@ final class LyricsManiaTest extends \aportela\ScraperLyrics\Test\BaseTest
     public function testScrapSuccess(): void
     {
         $success = self::$lyrics->scrap("Bohemian Rhapsody", "Queen", [\aportela\ScraperLyrics\SourceProvider::LYRICS_MANIA]);
-        // I suspect that sometimes BING fail scrap on first call (next are working)
-        // At this time, I don't know if it is due to some protection method of the server itself or if it is waiting for some type of previous redirection / cookie set
-        // I tried getting cookies with HEAD (Previous to GET), sending Referer header, faking User Agent to Edge, nothing works 100%
-        // ... SO retry scrap if first failed... (I know, this sucks)
-        if (!$success) {
-            $success = self::$lyrics->scrap("Bohemian Rhapsody", "Queen", [\aportela\ScraperLyrics\SourceProvider::LYRICS_MANIA]);
-        }
         $this->assertTrue($success);
         $this->assertNotEmpty(self::$lyrics->lyrics);
         $this->assertEquals(self::$lyrics->source, "lyricsmania");
