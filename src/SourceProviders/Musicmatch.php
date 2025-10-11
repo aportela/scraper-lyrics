c66dc512033316d518dc6d04af755209182b3058
alex <766f6964+github@gmail.com>
2025-10-11 11:24:18 +0200

    scrap fixed for musicmatch provider

--- src/SourceProviders/Musicmatch.php
+++ src/SourceProviders/Musicmatch.php
@@ -10,46 +10,15 @@ final class Musicmatch extends BaseProvider
         parent::__construct($logger, self::USER_AGENT);
     }
 
-    private function getLink(string $title, string $artist): string
-    {
-        $this->http->setReferer("https://www.musixmatch.com/");
-        $response = $this->http->GET(sprintf("https://www.musixmatch.com/search/%s/tracks", urlencode($title . " " . $artist)));
-        if ($response->code == 200) {
-            if (!empty($response->body)) {
-                libxml_use_internal_errors(true);
-                $doc = new \DomDocument();
-                if ($doc->loadHTML($response->body)) {
-                    $xpath = new \DOMXPath($doc);
-                    // lyric links are contained on multiple childs of <ul class="tracks list thumb-list">
-                    $nodes = $xpath->query('//ul[@class="tracks list thumb-list"]/li/div[@class="track-card media-card has-picture"]/div[@class="media-card-body"]/div[@class="media-card-text"]/h2/a');
-                    if ($nodes != false) {
-                        if ($nodes->count() > 0) {
-                            return ($nodes[0]->getAttribute('href'));
-                        } else {
-                            throw new \aportela\ScraperLyrics\Exception\InvalidSourceProviderAPIResponse(sprintf("HTML Nodes %s not found", '//div[@class="lyrics-body"]'));
-                        }
-                    } else {
-                        throw new \aportela\ScraperLyrics\Exception\InvalidSourceProviderAPIResponse(sprintf("HTML Nodes %s not found", '//div[@class="lyrics-body"]'));
-                    }
-                } else {
-                    throw new \aportela\ScraperLyrics\Exception\InvalidSourceProviderAPIResponse("Invalid HTML body");
-                }
-            } else {
-                throw new \aportela\ScraperLyrics\Exception\HTTPException("Invalid HTTP (empty) body");
-            }
-        } else {
-            throw new \aportela\ScraperLyrics\Exception\HTTPException("Invalid HTTP response code: " . $response->code);
-        }
-    }
-
     public function scrap(string $title, string $artist): string
     {
         // get cookies & set referer so that Musicmatch does not realize so easily that we are using a script and not a browser
         // I can't guarantee it will always work, but the request will be less suspicious in a quick analysis
         $response = $this->http->HEAD("https://www.musixmatch.com/");
-        $link = $this->getLink($title, $artist);
-        $this->http->setReferer(sprintf("https://www.musixmatch.com/search/%s/tracks", urlencode($title . " " . $artist)));
-        $response = $this->http->GET(sprintf("https://www.musixmatch.com%s/embed", $link));
+        // "simulate" that we came to this from google search
+        $this->http->setReferer("https://www.google.com/search?client=firefox-b-d&q=" . urlencode("musicmatch lyrics " . $title . " " . $artist));
+        //$response = $this->http->GET(sprintf("https://www.musixmatch.com%s/embed", $link));
+        $response = $this->http->GET(sprintf("https://www.musixmatch.com/lyrics/%s/%s", str_replace(" ", "-", $artist), str_replace(" ", "-", $title)));
         if ($response->code == 200) {
             if (!empty($response->body)) {
                 libxml_use_internal_errors(true);
@@ -57,7 +26,7 @@ final class Musicmatch extends BaseProvider
                 if ($doc->loadHTML(str_ireplace(array("<br>", "<br/>", "<br />"), PHP_EOL, $response->body))) {
                     $xpath = new \DOMXPath($doc);
                     // lyric paragraphs are contained on multiple p childs of <div class="track-widget-body">
-                    $nodes = $xpath->query('//div[@class="track-widget-body"]/p');
+                    $nodes = $xpath->query('//div[contains(@class, "r-11rrj2j") and contains(@class, "r-15zivkp") and @dir="auto"]');
                     if ($nodes != false) {
                         if ($nodes->count() > 0) {
                             $data = null;
@@ -71,10 +40,10 @@ final class Musicmatch extends BaseProvider
                                 throw new \aportela\ScraperLyrics\Exception\NotFoundException("");
                             }
                         } else {
-                            throw new \aportela\ScraperLyrics\Exception\InvalidSourceProviderAPIResponse(sprintf("HTML Nodes %s not found", '//div[@class="track-widget-body"]/p'));
+                            throw new \aportela\ScraperLyrics\Exception\InvalidSourceProviderAPIResponse(sprintf("HTML Nodes %s not found", '//div[@class="css-146c3p1 r-1inkyih r-11rrj2j r-13awgt0 r-fdjqy7 r-1dxmaum r-1it3c9n r-135wba7"]'));
                         }
                     } else {
-                        throw new \aportela\ScraperLyrics\Exception\InvalidSourceProviderAPIResponse(sprintf("HTML Nodes %s not found", '//div[@class="track-widget-body"]/p'));
+                        throw new \aportela\ScraperLyrics\Exception\InvalidSourceProviderAPIResponse(sprintf("HTML Nodes %s not found", '//div[@class="css-146c3p1 r-1inkyih r-11rrj2j r-13awgt0 r-fdjqy7 r-1dxmaum r-1it3c9n r-135wba7"]'));
                     }
                 } else {
                     throw new \aportela\ScraperLyrics\Exception\InvalidSourceProviderAPIResponse("Invalid HTML body");
