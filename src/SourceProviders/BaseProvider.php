a60af79357dca7bde702e8d3c82b4182551340d9
alex <766f6964+github@gmail.com>
2025-11-03 14:41:19 +0100

    extension exceptions replaced

--- src/SourceProviders/BaseProvider.php
+++ src/SourceProviders/BaseProvider.php
@@ -2,35 +2,26 @@
 
 namespace aportela\ScraperLyrics\SourceProviders;
 
-class BaseProvider implements ISourceProvider
+abstract class BaseProvider implements ISourceProvider
 {
     protected \Psr\Log\LoggerInterface $logger;
     protected \aportela\HTTPRequestWrapper\HTTPRequest $http;
 
-    public function __construct(\Psr\Log\LoggerInterface $logger, ?string $customUserAgent)
+    public function __construct(\Psr\Log\LoggerInterface $logger, ?string $customUserAgent = null)
     {
         $this->logger = $logger;
-        $this->logger->debug("ScraperLyrics\SourceProviders\BaseProvider::__construct");
         $loadedExtensions = get_loaded_extensions();
         if (!in_array("libxml", $loadedExtensions)) {
-            $this->logger->critical("ScraperLyrics\SourceProviders\BaseProvider::__construct ERROR: libxml extension not found");
-            throw new \aportela\ScraperLyrics\Exception\LibXMLMissingException("loaded extensions: " . implode(", ", $loadedExtensions));
+            $this->logger->critical("\aportela\ScraperLyrics\SourceProviders\BaseProvider::__construct - Error: libxml extension not found", $loadedExtensions);
+            throw new \aportela\ScraperLyrics\Exception\ExtensionMissingException("missing libxml extension, loaded extensions: " . implode(", ", $loadedExtensions));
         } elseif (!in_array("SimpleXML", $loadedExtensions)) {
-            $this->logger->critical("ScraperLyrics\SourceProviders\BaseProvider::__construct ERROR: SimpleXML extension not found");
-            throw new \aportela\ScraperLyrics\Exception\SimpleXMLMissingException("loaded extensions: " . implode(", ", $loadedExtensions));
-        } else {
-            $this->logger->debug("ScraperLyrics\SourceProviders\BaseProvider::__construct");
-            $this->http = new \aportela\HTTPRequestWrapper\HTTPRequest($this->logger, $customUserAgent ?? "");
+            $this->logger->critical("MusicBrainzWrapper::__construct ERROR: SimpleXML extension not found", $loadedExtensions);
+            throw new \aportela\ScraperLyrics\Exception\ExtensionMissingException("missing simplexml extension, loaded extensions: " . implode(", ", $loadedExtensions));
         }
+        $this->http = new \aportela\HTTPRequestWrapper\HTTPRequest($this->logger, $customUserAgent);
     }
 
-    public function scrap(string $title, string $artist): string
-    {
-        return ("");
-    }
+    public abstract function scrap(string $title, string $artist): string;
 
-    public function __destruct()
-    {
-        $this->logger->debug("ScraperLyrics\SourceProviders\BaseProvider::__destruct");
-    }
+    public function __destruct() {}
 }
